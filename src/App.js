daed4f9143f767612478b20ac496ff2fb6b7a1ce
abdullahbinshahzad <abdullahbinshahzad228@gmail.com>
2025-06-11 19:05:08 +0500

    second commit 11-6-25

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import CreateNewPassword from "./pages/auth/CreateNewPassword";
@@ -8,10 +8,12 @@ import VerifyOTP from "./pages/auth/VerifyOTP";
 const App = () => {
   return (
     <Router>
-      <Route exact path="/" component={Login} />
-      <Route path="/forgot-password" component={ForgotPassword} />
-      <Route path="/create-new-password" component={CreateNewPassword} />
-      <Route path="/verify-otp" component={VerifyOTP} />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/create-new-password" element={<CreateNewPassword />} />
+        <Route path="/verify-otp" element={<VerifyOTP />} />
+      </Routes>
     </Router>
   );
 };
